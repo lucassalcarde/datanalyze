32c41a005c52eefe9a05276cf7bc2b1b7ce12b13
lucasalcarde <lucassalcarde@gmail.com>
2018-06-01 17:08:48 -0300

    criado função para criar e armazenar log em arquivo txt

---
+++ arquivo.py
@@ -0,0 +1,19 @@
+"""cria arquivo de log .txt."""
+import os.path
+
+
+def grava_arquivo(caminho, texto):
+    """Função grava arquivo."""
+    if caminho:
+        caminhoarq = caminho + '/log.txt'
+    else:
+        caminhoarq = 'log.txt'
+    if os.path.isfile(caminhoarq):
+        parametro = 'a'
+    else:
+        parametro = 'w'
+    arq = open(caminhoarq, parametro)
+    linha = ''
+    for linha in texto:
+        arq.write(linha + '\n')
+    arq.close
